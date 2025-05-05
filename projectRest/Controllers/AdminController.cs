c2b86c5b51bfaa67a4c63e57f3474e84cbcdd413
Ahmed Yousef El-shemy <ahmedyoussef699y@gmail.com>
2025-05-05 11:16:56 +0300

    Add admin Controller

---
+++ projectRest/Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace projectRest.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ResturantdbContext _Context;
+        public AdminController(ResturantdbContext context)
+        {
+            _Context = context;
+        }
+        public IActionResult OrdarList()
+        {
+            var ordersL = _Context.Orders.ToList();
+            return View(ordersL);
+        }
+
+        public IActionResult UserList()
+        {
+            var usersL = _Context.Customers.ToList();
+            return View(usersL);
+        }
+
+
+
+    }
+}
